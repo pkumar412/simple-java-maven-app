949bc08281318842730f19bf647d6fac0855cfa3
kumaraswamy.prashanth <kumaraswamy.prashanth@outlook.com>
2021-11-11 15:03:16 +0530

    test jenkins build

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!hanthuuuuuuu;
+    private final String message = "Hello World!hanthuuuuuuuA;
 
     public App() {}
 
